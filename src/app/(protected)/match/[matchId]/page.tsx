b1553bda8a1d367753d2598145ebc0677c57fc25
Edoardo <em5129@nyu.edu>
2026-01-30 00:24:43 -0500

    Fix: remove unused User import

--- src/app/(protected)/match/[matchId]/page.tsx
+++ src/app/(protected)/match/[matchId]/page.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { motion } from 'framer-motion';
 import {
-  User,
   MapPin,
   Navigation,
   Check,
