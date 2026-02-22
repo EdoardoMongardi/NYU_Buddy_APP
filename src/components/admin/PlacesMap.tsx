84e5871a06ff70b13d7e41fed2e73a98e6684c28
Edoardo <em5129@nyu.edu>
2026-02-21 19:07:25 -0500

    lint error fix

--- src/components/admin/PlacesMap.tsx
+++ src/components/admin/PlacesMap.tsx
@@ -35,7 +35,7 @@ export function PlacesMap({ places }: PlacesMapProps) {
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     const L = require('leaflet');
-    delete (L.Icon.Default.prototype as any)._getIconUrl;
+    delete (L.Icon.Default.prototype as typeof L.Icon.Default.prototype & { _getIconUrl?: unknown })._getIconUrl;
     L.Icon.Default.mergeOptions({
       iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
       iconUrl:       'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
